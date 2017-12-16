25655985c508a944adfbf038bd6ba9525f29a982
tw22885431 <re22885431@gmail.com>
2017-12-16 17:05:41 +0800

    test

--- DEMO/Form1.cs
+++ DEMO/Form1.cs
@@ -23,17 +23,13 @@ namespace DEMO
             this.Close();
         }
 
-
-        private void Form1_Load(object sender, EventArgs e)
+        private void label1_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void 註冊ToolStripMenuItem_Click(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
-            RegisterForm regsiter = new RegisterForm();
-
-            regsiter.Show();
 
         }
     }
